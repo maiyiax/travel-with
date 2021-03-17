86860a76d0498cf51bbd4d4808c6d1a4f4c7ba5b
Mai Yia Xiong <maiyiax@gmail.com>
2021-03-16 20:26:07 -0500

    added auth.js

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const { ApolloServer } = require('apollo-server-express');
 const path = require('path');
 
 const { typeDefs, resolvers } = require('./schema');
+const { authMiddleware } = require('./utils/auth');
 const db = require('./config/connection');
 
 const app = express();
@@ -11,10 +12,10 @@ const PORT = process.env.PORT || 3005;
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    //context: authMiddleware
+    context: authMiddleware
   });
 
-//server.applyMiddleware({ app });
+server.applyMiddleware({ app });
 
 
 app.use(express.json());
@@ -25,10 +26,9 @@ if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
   }
   
-// app.get('*', (req, res) => {
-
-//  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-//  });
+app.get('*', (req, res) => {
+ res.sendFile(path.join(__dirname, '../client/build/index.html'));
+ });
   
   db.once('open', () => {
     app.listen(PORT, () => {
